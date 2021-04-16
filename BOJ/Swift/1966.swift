2d92e0a822d2825f79978ba51c88db6f845ebe26
JiWon Lee <jiwon.swdev@gmail.com>
2021-04-16 23:08:00 +0900

    feat: BOJ #1966 - 프린터 큐

---
+++ BOJ/Swift/1966.swift
@@ -0,0 +1,20 @@
+let t = Int(readLine()!)!
+for _ in 0..<t {
+    let n = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var importance = readLine()!.split(separator: " ").enumerated().map { (index, element) in
+        return [Int(String(element))!, index]}
+    var answer = [[Int]]()
+    while !importance.isEmpty {
+        if let first = importance.first, let max = importance.max(by: { $0[0] < $1[0] }) {
+            if first[0] == max[0] {
+                answer.append(importance.removeFirst())
+            } else {
+                importance.append(importance.removeFirst())
+            }
+        }
+    }
+
+    if let find = answer.firstIndex(where: { $0[1] == n[1] }) {
+        print(find + 1)
+    }
+}
