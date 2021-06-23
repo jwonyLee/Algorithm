e17ab83656f748546631485cfda292a1c3138edf
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-23 22:53:39 +0900

    boj: #5648 - 역원소 정렬

---
+++ BOJ/Swift/5648.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10 && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+
+        return str
+    }
+}
+
+var fileio = FileIO()
+
+let n = fileio.readInt()
+var nums = [String]()
+for _ in 0..<n {
+    let num = String(String(fileio.readInt()).reversed()).replacingOccurrences(of: "^0+", with: "", options: .regularExpression)
+    nums.append(num)
+}
+nums.sort(by: { $0.localizedStandardCompare($1) == .orderedAscending })
+for num in nums {
+    print(num)
+}
