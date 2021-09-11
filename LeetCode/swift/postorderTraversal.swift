117db6ed6543a05b12282a5a308cba3ffc8d94c3
JiWon Lee <jiwon.swdev@gmail.com>
2021-09-12 00:06:28 +0900

    LeetCode: Binary Tree Postorder Traversal

---
+++ LeetCode/swift/postorderTraversal.swift
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    var answer: [Int] = []
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        traversal(root)
+        return answer
+    }
+    
+    func traversal(_ root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        traversal(root.left)
+        traversal(root.right)
+        answer.append(root.val)
+    }
+}
