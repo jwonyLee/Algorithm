b650ca8e33d8fd770cbb5360beae8c82be358a8e
JiWon Lee <jiwon.swdev@gmail.com>
2021-09-15 16:58:13 +0900

    LeetCode: Intersection of Two Arrays

---
+++ LeetCode/swift/intersection.swift
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/intersection-of-two-arrays/
+class Solution {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        return Array(Set(nums1).intersection(Set(nums2)))
+    }
+}
